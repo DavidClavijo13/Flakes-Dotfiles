8c5266f4e9c924fdbe3c6a3067d0ea46380a47d5
DavidClavijo13 <davichodavid13@gmail.com>
2025-07-29 15:15:20 -0400

    meh

--- modules/home-manager-config.nix
+++ modules/home-manager-config.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }:
 
 let
-  # where your real nvim config lives in the flake
+  # Path to your dotfiles’ nvim folder
   nvimSrc = ../files/nvim;
 in {
   ###########################
@@ -16,14 +16,13 @@ in {
   ###########################
   programs.zsh = {
     enable = true;
-    # after HM’s own setup, drop into your legacy .zshrc
     initExtra = ''
       source ${../files/.zshrc}
     '';
   };
 
   ###########################
-  # User‐level Packages
+  # User-level Packages
   ###########################
   home.packages = with pkgs; [
     git zsh neovim ghostty wl-clipboard fzf zoxide jq bc gawk
@@ -36,21 +35,18 @@ in {
   home.file = {
     ".p10k.zsh"       = { source = ../files/.p10k.zsh; };
 
-    # recursive = true pulls in the entire folder tree
     ".config/ghostty" = { source = ../files/ghostty; recursive = true; };
     ".config/hypr"    = { source = ../files/hypr;    recursive = true; };
     ".config/waybar"  = { source = ../files/waybar;  recursive = true; };
     ".config/nvim"    = { source = ../files/nvim;    recursive = true; };
   };
 
-  ########################################
-  # Activation: copy nvim into a writeable directory
-  ########################################
-  home.activation.copyNvimConfig = lib.dag.entryAfter [ "writeBoundary" ] ''
-    # remove any old symlink or dir
+  ############################################
+  # Activation: copy nvim into a writeable dir
+  ############################################
+  home.activation.copyNvimConfig = lib.mkAfter "writeBoundary" ''
     rm -rf "$HOME/.config/nvim"
     mkdir -p "$HOME/.config"
-    # copy *your* config into place as a normal folder
     cp -r ${nvimSrc} "$HOME/.config/nvim"
   '';
 }
