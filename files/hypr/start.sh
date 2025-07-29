fb58f3a3d8d193da815f9dbae71f40d5da658230
DavidClavijo13 <davichodavid13@gmail.com>
2025-07-29 17:36:06 -0400

    startsh

--- files/hypr/start.sh
+++ files/hypr/start.sh
@@ -1,17 +1,30 @@
 #!/usr/bin/env bash
+# ~/.config/hypr/start.sh
+#
+# Updated to ensure swww has time to initialize before setting the wallpaper.
+
+# 1) Launch the swww daemon in the background
 swww init &
 
-swww img /home/logonix/Downloads/wp4472154-5120x2160-wallpapers.jpg &
+# 2) Determine the runtime directory (fallback if XDG_RUNTIME_DIR isn't set)
+runtime_dir="${XDG_RUNTIME_DIR:-/run/user/$(id -u)}"
 
-nm-applet --indicator & 
+# 3) Wait up to 1 second for the swww socket to appear
+for i in {1..10}; do
+  if [ -e "${runtime_dir}/swww.sock" ]; then
+    break
+  fi
+  sleep 0.1
+done
 
+# 4) Now set your wallpaper
+swww img "${HOME}/Downloads/wp4472154-5120x2160-wallpapers.jpg" &
+
+# 5) Launch the rest of your apps
+nm-applet --indicator &
 waybar &
-
 mako &
-
 flatpak run app.zen_browser.zen &
-
 discord &
-
 ghostty &
 
