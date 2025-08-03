3f1c39de2616d883ea6b791a38cc36c14df7c1b3
DavidClavijo13 <davichodavid13@gmail.com>
2025-08-03 15:20:17 -0400

    startup window sizing

--- files/hypr/start.sh
+++ files/hypr/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 export PATH="$HOME/.nix-profile/bin:/run/current-system/profile/bin:$PATH"
 
-# Launch background apps
+# Start background apps
 swww-daemon --format xrgb &
 sleep 0.2
 swww img --transition-type simple "$HOME/Downloads/wp4472154-5120x2160-wallpapers.jpg" &
@@ -12,37 +12,47 @@ mako &
 # Launch apps
 ghostty &
 sleep 0.5
+
 ghostty &
 sleep 0.5
+
 flatpak run app.zen_browser.zen &
 sleep 1
+
 discord &
-sleep 2
+sleep 3  # Give Discord more time
 
 # Get window addresses
 mapfile -t ghostties < <(hyprctl clients -j | jq -r '.[] | select(.class == "com.mitchellh.ghostty") | .address')
 zen=$(hyprctl clients -j | jq -r '.[] | select(.class == "zen") | .address')
-discord=$(hyprctl clients -j | jq -r '.[] | select(.class == "discord") | sort_by(.size[0]) | reverse | .address' | head -n 1)
 
-# Float each explicitly
-hyprctl dispatch togglefloating address:$discord
+discord=$(hyprctl clients -j | jq -r '
+  .[] | select(.class == "discord" and .title | test("Discord|Friends")) | 
+  sort_by(.size[0]) | last | .address')
+
+# Make sure Discord is floating
+if [[ "$(hyprctl clients -j | jq -r --arg addr "$discord" '.[] | select(.address == $addr) | .floating')" != "true" ]]; then
+  hyprctl dispatch togglefloating address:$discord
+fi
+
+# Float the others
 hyprctl dispatch togglefloating address:$zen
 hyprctl dispatch togglefloating address:${ghostties[0]}
 hyprctl dispatch togglefloating address:${ghostties[1]}
 
-# 💡 Move Discord first
+# Position Discord first (far right)
 hyprctl dispatch movewindowpixel exact 3596 48,address:$discord
 hyprctl dispatch resizewindowpixel exact 1518 2106,address:$discord
 
-# Then Zen
+# Then Zen (middle)
 hyprctl dispatch movewindowpixel exact 1566 48,address:$zen
 hyprctl dispatch resizewindowpixel exact 2018 2106,address:$zen
 
-# Then Ghostty top
+# Ghostty top left
 hyprctl dispatch movewindowpixel exact 6 48,address:${ghostties[0]}
 hyprctl dispatch resizewindowpixel exact 1548 1047,address:${ghostties[0]}
 
-# Then Ghostty bottom
+# Ghostty bottom left
 hyprctl dispatch movewindowpixel exact 6 1107,address:${ghostties[1]}
 hyprctl dispatch resizewindowpixel exact 1548 1047,address:${ghostties[1]}
 
