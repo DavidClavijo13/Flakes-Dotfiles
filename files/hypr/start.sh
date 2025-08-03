e8b09b9b8393e6ca5d8cf8cfc2878dfa9075a4a5
DavidClavijo13 <davichodavid13@gmail.com>
2025-08-03 15:11:10 -0400

    startup window sizing

--- files/hypr/start.sh
+++ files/hypr/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 export PATH="$HOME/.nix-profile/bin:/run/current-system/profile/bin:$PATH"
 
-# Start background services
+# Launch background services
 swww-daemon --format xrgb &
 sleep 0.2
 swww img --transition-type simple "$HOME/Downloads/wp4472154-5120x2160-wallpapers.jpg" &
@@ -9,7 +9,7 @@ nm-applet --indicator &
 ~/.config/hypr/toggle-waybar.sh &
 mako &
 
-# Launch startup apps
+# Launch startup windows
 ghostty &
 sleep 0.5
 
@@ -20,30 +20,31 @@ flatpak run app.zen_browser.zen &
 sleep 1
 
 discord &
-sleep 1
+sleep 2  # Discord can be slow to fully appear
 
-# Gather window addresses
+# Gather window IDs
 mapfile -t ghostties < <(hyprctl clients -j | jq -r '.[] | select(.class == "com.mitchellh.ghostty") | .address')
 zen=$(hyprctl clients -j | jq -r '.[] | select(.class == "zen") | .address')
-discord=$(hyprctl clients -j | jq -r '.[] | select(.class == "discord") | sort_by(.at[0]) | last | .address')
+discord=$(hyprctl clients -j | jq -r '.[] | select(.class == "discord") | sort_by(.size[0]) | reverse | .address' | head -n 1)
 
-# Position ghostty windows
+# Ensure all windows are explicitly floating
+hyprctl dispatch togglefloating address:${ghostties[0]}
+hyprctl dispatch togglefloating address:${ghostties[1]}
+hyprctl dispatch togglefloating address:$zen
+hyprctl dispatch togglefloating address:$discord
+
+# Place ghostties
 hyprctl dispatch movewindowpixel exact 6 48,address:${ghostties[0]}
 hyprctl dispatch resizewindowpixel exact 1548 1047,address:${ghostties[0]}
 
 hyprctl dispatch movewindowpixel exact 6 1107,address:${ghostties[1]}
 hyprctl dispatch resizewindowpixel exact 1548 1047,address:${ghostties[1]}
 
-# Position Zen browser
+# Place Zen
 hyprctl dispatch movewindowpixel exact 1566 48,address:$zen
 hyprctl dispatch resizewindowpixel exact 2018 2106,address:$zen
 
-# Position Discord
+# Place Discord
 hyprctl dispatch movewindowpixel exact 3596 48,address:$discord
 hyprctl dispatch resizewindowpixel exact 1518 2106,address:$discord
-
-# Remove float rules so future apps tile normally
-hyprctl keyword windowrulev2 remove "float,class:(com.mitchellh.ghostty)"
-hyprctl keyword windowrulev2 remove "float,class:(zen)"
-hyprctl keyword windowrulev2 remove "float,class:(discord)"
 
