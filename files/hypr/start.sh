caad28c469f90bdf9e278a7fc359ff0db12a5c49
DavidClavijo13 <davichodavid13@gmail.com>
2025-08-03 15:08:06 -0400

    startup window sizing

--- files/hypr/start.sh
+++ files/hypr/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 export PATH="$HOME/.nix-profile/bin:/run/current-system/profile/bin:$PATH"
 
-# Wallpaper setup
+# Start background services
 swww-daemon --format xrgb &
 sleep 0.2
 swww img --transition-type simple "$HOME/Downloads/wp4472154-5120x2160-wallpapers.jpg" &
@@ -9,7 +9,7 @@ nm-applet --indicator &
 ~/.config/hypr/toggle-waybar.sh &
 mako &
 
-# Launch everything
+# Launch startup apps
 ghostty &
 sleep 0.5
 
@@ -22,21 +22,28 @@ sleep 1
 discord &
 sleep 1
 
-# Get window addresses in the order they actually appeared
+# Gather window addresses
 mapfile -t ghostties < <(hyprctl clients -j | jq -r '.[] | select(.class == "com.mitchellh.ghostty") | .address')
 zen=$(hyprctl clients -j | jq -r '.[] | select(.class == "zen") | .address')
-discord=$(hyprctl clients -j | jq -r '.[] | select(.class == "discord") | .address')
+discord=$(hyprctl clients -j | jq -r '.[] | select(.class == "discord") | sort_by(.at[0]) | last | .address')
 
-# Reorder if needed (ghostties[0] should be top, ghostties[1] bottom)
+# Position ghostty windows
 hyprctl dispatch movewindowpixel exact 6 48,address:${ghostties[0]}
 hyprctl dispatch resizewindowpixel exact 1548 1047,address:${ghostties[0]}
 
 hyprctl dispatch movewindowpixel exact 6 1107,address:${ghostties[1]}
 hyprctl dispatch resizewindowpixel exact 1548 1047,address:${ghostties[1]}
 
+# Position Zen browser
 hyprctl dispatch movewindowpixel exact 1566 48,address:$zen
 hyprctl dispatch resizewindowpixel exact 2018 2106,address:$zen
 
+# Position Discord
 hyprctl dispatch movewindowpixel exact 3596 48,address:$discord
 hyprctl dispatch resizewindowpixel exact 1518 2106,address:$discord
+
+# Remove float rules so future apps tile normally
+hyprctl keyword windowrulev2 remove "float,class:(com.mitchellh.ghostty)"
+hyprctl keyword windowrulev2 remove "float,class:(zen)"
+hyprctl keyword windowrulev2 remove "float,class:(discord)"
 
